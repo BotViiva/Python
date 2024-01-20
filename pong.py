8aa434b8646ec2ffe954e7ca0f00854373270868
Jaakko <jaakko.niininen@gmail.com>
2024-01-20 19:58:47 +0200

    koodia siistitty, lisätty aikaviive pallon mennessä maaliin

--- pong.py
+++ pong.py
@@ -18,7 +18,8 @@ def start_screen ():
     window.blit(title_text, title_rect)
     window.blit(start_text, start_rect)
     window.blit(controls_text, controls_rect)
-    window.blit(controls_info_text, controls_info_rect)
+    window.blit(p1_controls_info_text, p1_controls_info_rect)
+    window.blit(p2_controls_info_text, p2_controls_info_rect)
     window.blit(start_space_text, start_space_rect)
     
 
@@ -29,24 +30,34 @@ window_size = (800,600)
 window = pygame.display.set_mode((window_size))
 pygame.display.set_caption('Pong')
 FPS = pygame.time.Clock()
+
 run = True
 game_active = False
 text_font_big = pygame.font.Font('assets/Grand9K.ttf', 70)
 text_font_medium = pygame.font.Font('assets/Grand9K.ttf', 30)
-text_font_small = pygame.font.Font('assets/Grand9K.ttf', 10)
+text_font_small = pygame.font.Font('assets/Grand9K.ttf', 20)
 scoreboard = [0, 0]
 
 #aloitusnäytön muuttujat
+p1_controls = "ArrowUp = up, ArrowDown = down"
+p2_controls = "W = up, S = down"
+
 title_text = text_font_big.render('Bong!', False, (255, 255, 255))
 title_rect = title_text.get_rect(center = (window_size[0] // 2, window_size[1] // 8))
+
 start_text = text_font_medium.render('START', False, (255, 255, 255))
 start_rect = start_text.get_rect(center = (window_size[0] // 2, window_size[1] - 100))
+
 start_space_text = text_font_small.render('Push space to start', False , (255, 255, 255))
 start_space_rect = start_space_text.get_rect( center = (window_size[0] // 2, window_size[1] - 140))
+
 controls_text = text_font_medium.render('Controls:', False, (255, 255, 255))
 controls_rect = controls_text.get_rect( center = (window_size[0] // 2, window_size[1] // 3))
-controls_info_text = text_font_small.render('W = up, S = down           ArrowUp = up, ArrowDown = down', False, (255, 255, 255))
-controls_info_rect = controls_info_text.get_rect(center = (window_size[0] // 2, controls_rect[2] + 100))
+
+p1_controls_info_text = text_font_small.render(p1_controls, False, (255, 255, 255))
+p2_controls_info_text = text_font_small.render(p2_controls, False, (255, 255, 255))
+p1_controls_info_rect = p1_controls_info_text.get_rect(center = (window_size[0] // 2 + 200, controls_rect[2] + 100))
+p2_controls_info_rect = p2_controls_info_text.get_rect(center = (window_size[0] // 2 - 200, controls_rect[2] + 100))
 
 #peliobjektit
 paddle_width, paddle_height = (10,100)
@@ -84,7 +95,7 @@ while run:
     if keys[pygame.K_s] and p2_paddle.bottom < window_size[1]:
         p2_paddle.y += 5
 
-    
+
 
     #tarkista osuiko pallo pohjaan 
     if ball.bottom > window_size[1]:
@@ -97,27 +108,29 @@ while run:
         ball = pygame.Rect(ball.x, 0 + ball_size , ball_size, ball_size)
 
     #tarkista osuiko pallo maaliin
-    if ball.x >= window_size[0] - ball_size:
+    if ball.x > window_size[0] - ball_size:
         ball = pygame.Rect(window_size[0] // 2 - ball_size // 2, window_size[1] // 2 - ball_size // 2, ball_size, ball_size)
         scoreboard[1] = scoreboard[1] + 1
-        print(scoreboard)
+        pygame.time.delay(2000)
 
-    if ball.x <= 0 :
+    if ball.x < 0 :
         ball = pygame.Rect(window_size[0] // 2 - ball_size // 2, window_size[1] // 2 - ball_size // 2, ball_size, ball_size)
         scoreboard[0] = scoreboard[0] + 1
-        print(scoreboard)
-        
+        pygame.time.delay(2000)
+
+
+
     #tarkista osuuko pallo oikeaan mailaan
     if ball.colliderect(p1_paddle):
         
         #muuttuja osuman sijainnille (tulos = -5/-4/-3/-2/-1/0/1/2/3/4/5/6)
         collision_area = (ball.centery - p1_paddle.top + ball_size // 2 -50) // 10
+        
         #muuta suuntaa
         ball_speed_x = -ball_speed_x
         
         #muuta nopeutta
         ball_speed_y = collision_area + ball_speed_y
-        print(collision_area, ball_speed_x, ball_speed_y)
         
         #tarkista maksiminopeus
         if ball_speed_y > 10:
@@ -128,6 +141,7 @@ while run:
             ball_speed_x = 6
         if ball_speed_x < -6:
             ball_speed_x = -6
+
     #tarkista osuuko pallo vasempaan mailaan
     if ball.colliderect(p2_paddle):
         
@@ -137,7 +151,6 @@ while run:
         ball_speed_x = -ball_speed_x
         # muuta nopeutta
         ball_speed_y = collision_area + ball_speed_y
-        print(collision_area, ball_speed_x, ball_speed_y)
     
         #tarkista maksiminopeus
         if ball_speed_y > 10:
@@ -152,6 +165,7 @@ while run:
     #päivitä näyttö
     pygame.display.flip()
 
+    #piirrä aloitusnäyttö
     if game_active == False:
         
         scoreboard = [0,0]
@@ -163,24 +177,28 @@ while run:
         
         
 
-    #piirrä elementit
+    #piirrä itse peli
     if game_active:
 
         
-        ball.x += ball_speed_x
-        ball.y += ball_speed_y
         window.fill((0, 0, 0))
         pygame.draw.line(window, (255, 255, 255), (window_size[0] // 2, 0), (window_size[0] // 2, window_size[1]))
         pygame.draw.rect(window, (255, 255, 255), p2_paddle)
         pygame.draw.rect(window, (255, 255, 255), p1_paddle)
+        
         pygame.draw.ellipse(window, (255, 255, 255), ball)
         score()
+        ball.x += ball_speed_x
+        ball.y += ball_speed_y
     
     if scoreboard[0] == 3:
         game_active = False
     if scoreboard[1] == 3:
         game_active = False
 
+    
+    
+    
     #rajoittaa framet 60:een
     FPS.tick(60)
 
