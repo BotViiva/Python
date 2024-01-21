cd9c6bca87b1cb1882bce262537d9ca8f447544d
Jaakko <jaakko.niininen@gmail.com>
2024-01-21 18:02:33 +0200

    mailat ja pallonnopeus asetettu uudelleen uuden pelin alkaessa, countdown jokaisen pallon alussa

--- pong.py
+++ pong.py
@@ -1,4 +1,3 @@
-
 import pygame;
 from sys import exit
 
@@ -10,6 +9,27 @@ def score ():
     score_p2_rect = score_p2_text.get_rect(center = (window_size[0] // 2 + 70, 50))
     window.blit(score_p1_text, score_p1_rect)
     window.blit(score_p2_text, score_p2_rect)
+    
+
+#pelin aloitus
+def game_start():
+    countdown = [3, 2, 1, "GO!"]
+    
+
+    for index in countdown:
+        window.fill((0, 0, 0,))
+        countdown_surf = text_font_big.render(str(index), False, (255, 255, 255))
+        countdown_rect = countdown_surf.get_rect(center = (window_size[0] // 2, window_size[1] // 2))
+        window.blit(countdown_surf, countdown_rect)
+        pygame.draw.line(window, (255, 255, 255), (window_size[0] // 2, 0), (window_size[0] // 2, window_size[1]))
+        pygame.draw.rect(window, (255, 255, 255), p2_paddle)
+        pygame.draw.rect(window, (255, 255, 255), p1_paddle)
+        pygame.draw.ellipse(window, (255, 255, 255), ball)
+        pygame.display.update()
+        pygame.time.wait(1000)
+        print(index)
+        if index == "GO!":
+            break
     
 
 #aloitusnäyttö
@@ -29,6 +49,7 @@ pygame.init()
 
 window_size = (800,600)
 window = pygame.display.set_mode((window_size))
+window_rect = pygame.Rect(0,0,window_size[0],window_size[1])
 pygame.display.set_caption('Pong')
 FPS = pygame.time.Clock()
 
@@ -56,6 +77,8 @@ start_space_rect = start_space_text.get_rect( center = (window_size[0] // 2, win
 controls_text = text_font_medium.render('Controls:', False, (255, 255, 255))
 controls_rect = controls_text.get_rect( center = (window_size[0] // 2, window_size[1] // 3))
 
+
+
 p1_controls_info_text = text_font_small.render(p1_controls, False, (255, 255, 255))
 p2_controls_info_text = text_font_small.render(p2_controls, False, (255, 255, 255))
 p1_controls_info_rect = p1_controls_info_text.get_rect(center = (window_size[0] // 2 + 200, controls_rect[2] + 100))
@@ -63,9 +86,9 @@ p2_controls_info_rect = p2_controls_info_text.get_rect(center = (window_size[0] 
 
 #peliobjektit
 paddle_width, paddle_height = (10,100)
-ball_size = (16)
-ball_speed_x = (3)
-ball_speed_y = (2)
+ball_size = 16
+ball_speed_x = 3
+ball_speed_y = 2
 p1_paddle = pygame.Rect(window_size[0] - paddle_width, window_size[1] // 2 - paddle_height // 2, paddle_width, paddle_height)
 p2_paddle = pygame.Rect(0, window_size[1] // 2 - paddle_height // 2, paddle_width, paddle_height)
 ball = pygame.Rect(window_size[0] // 2 - ball_size // 2, window_size[1] // 2 - ball_size // 2, ball_size, ball_size)
@@ -73,6 +96,7 @@ ball_hit_times = 0
 
 
 while run:
+
     #tapa poistua loopista
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -80,6 +104,7 @@ while run:
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN and start_rect.collidepoint(pygame.mouse.get_pos()) :
             game_active = True
+            game_start()
 
 
     #keys muuttuja (key.get_pressed() tarkkailee kaikkia painettuja nappeja) 
@@ -107,21 +132,31 @@ while run:
     #tarkista osuiko pallo kattoon
     if ball.top <= 0 :
         ball_speed_y = -ball_speed_y
-        ball = pygame.Rect(ball.x, 0 + ball_size , ball_size, ball_size)
+        ball = pygame.Rect(ball.x, 0 + ball_size // 2 , ball_size, ball_size)
 
     #tarkista osuiko pallo maaliin
+    
+    #oikea maali
     if ball.x > window_size[0] - ball_size:
-        ball = pygame.Rect(window_size[0] // 2 - ball_size // 2, window_size[1] // 2 - ball_size // 2, ball_size, ball_size)
         scoreboard[1] = scoreboard[1] + 1
-        pygame.time.delay(1000)
-        ball_hit_times = 0
-
-    if ball.x < 0 :
         ball = pygame.Rect(window_size[0] // 2 - ball_size // 2, window_size[1] // 2 - ball_size // 2, ball_size, ball_size)
-        scoreboard[0] = scoreboard[0] + 1
-        pygame.time.delay(1000)
+        pygame.time.delay(500)
         ball_hit_times = 0
+        ball_speed_x = 3
+        if  scoreboard[1] < 3:
+            print("mjääy")
+            game_start()
 
+    #vasen maali
+    if ball.x < 0 :
+        scoreboard[0] = scoreboard[0] + 1
+        ball = pygame.Rect(window_size[0] // 2 - ball_size // 2, window_size[1] // 2 - ball_size // 2, ball_size, ball_size)
+        pygame.time.delay(500)
+        ball_hit_times = 0
+        ball_speed_x = -3
+        if scoreboard[0] < 3:
+            print("mijau")
+            game_start()
 
     #tarkista osuuko pallo oikeaan mailaan
     if ball.colliderect(p1_paddle):
@@ -189,23 +224,28 @@ while run:
     #piirrä aloitusnäyttö
     if game_active == False:
         
+        #nollaa edellisen pelin tiedot
+        p1_paddle = pygame.Rect(window_size[0] - paddle_width, window_size[1] // 2 - paddle_height // 2, paddle_width, paddle_height)
+        p2_paddle = pygame.Rect(0, window_size[1] // 2 - paddle_height // 2, paddle_width, paddle_height)
         scoreboard = [0,0]
+        ball_speed_x = 3
+        ball_speed_y = 2
         
         start_screen()
 
         if keys[pygame.K_SPACE] :
             game_active = True
-        
-        
+            game_start()
 
     #piirrä peli
     if game_active:
+        
+        
 
         window.fill((0, 0, 0))
         pygame.draw.line(window, (255, 255, 255), (window_size[0] // 2, 0), (window_size[0] // 2, window_size[1]))
         pygame.draw.rect(window, (255, 255, 255), p2_paddle)
         pygame.draw.rect(window, (255, 255, 255), p1_paddle)
-        
         pygame.draw.ellipse(window, (255, 255, 255), ball)
         score()
         ball.x += ball_speed_x
